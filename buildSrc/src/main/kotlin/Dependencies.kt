5602bf5328a205f842d9a1fb13adc4f46f14355b
Hidetsugu Tamaki <hidetsugu.tamaki@linecorp.com>
2021-07-27 16:47:08 +0900

    Update AGP to 4.2.2

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -22,7 +22,7 @@ object Dependencies {
         const val kotlin = "1.3.72"
 
         // Plugin
-        const val androidPlugin = "4.1.3"
+        const val androidPlugin = "4.2.2"
         const val gradlePublishPlugin = "0.14.0"
         const val ktlintGradlePlugin = "10.0.0"
         const val gradleVersionsPlugin = "0.38.0"
