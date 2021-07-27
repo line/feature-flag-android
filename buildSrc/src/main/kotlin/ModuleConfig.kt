c87c6a12341e7100d96409a888bee6227b1f1bf8
Hidetsugu Tamaki <hidetsugu.tamaki@linecorp.com>
2021-07-27 17:13:49 +0900

    Bump up version to 2.4.0

--- buildSrc/src/main/kotlin/ModuleConfig.kt
+++ buildSrc/src/main/kotlin/ModuleConfig.kt
@@ -1,5 +1,5 @@
 object ModuleConfig {
-    const val version = "2.3.0"
+    const val version = "2.4.0"
     const val groupId = "com.linecorp.android"
     const val pluginId = "com.linecorp.android.feature-flag"
 }
