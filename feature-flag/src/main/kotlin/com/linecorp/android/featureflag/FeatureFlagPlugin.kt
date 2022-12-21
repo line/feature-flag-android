af07487852fe545850dc63b864880997f02377fa
YOUNG CHA <ganachoco@linecorp.com>
2022-12-21 14:26:50 +0900

    Remove unused import

--- feature-flag/src/main/kotlin/com/linecorp/android/featureflag/FeatureFlagPlugin.kt
+++ feature-flag/src/main/kotlin/com/linecorp/android/featureflag/FeatureFlagPlugin.kt
@@ -30,7 +30,6 @@ import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.Task
 import org.gradle.kotlin.dsl.create
-import org.gradle.util.GradleVersion
 import java.io.File
 
 /**
